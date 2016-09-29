672ab02dc053e43cf1bc971f1cd4686987ede5da
GallupinGallup <gallupingallup@gmail.com>
2016-09-29 13:32:02 -0600

    Made thingy lists

--- src/popups/controller/Controller.java
+++ src/popups/controller/Controller.java
@@ -1,23 +1,78 @@
 package popups.controller;
 
 import popups.view.Popupsviewer;
+import popups.model.Thingy;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Controller
 {
 	private Popupsviewer display;
-	
-	public Controller(){
+	private List<Thingy> thingyList;
+
+	public Controller()
+	{
 		display = new Popupsviewer();
+		thingyList = new ArrayList<Thingy>();
 	}
+
 	public void start()
 	{
+		
+	}
+	private void askQuestionLoop()
+	{
 		String answer = "sample";
-		while(!answer.equals(""))
+		
+		while (answer != null &&!isDouble(answer))
 		{
-			display.displayMessage("look words on the monitor");
-			
-			answer = display.collectResponse("Who are you?");
-			
+			answer = display.collectResponse("You need to type a doubble");
 		}
 	}
+
+	private boolean isDouble(String potentialValue)
+	{
+		boolean validDouble = false;
+		
+		try
+		{
+			double testvalue = Double.parseDouble(potentialValue);
+			validDouble = true;
+		}
+		catch(NumberFormatException exception)
+		{
+			exception.printStackTrace();
+			display.displayMessage("That was not a double =:<");
+		}
+		return validDouble;
+		/**
+		 * checks the supplied String value to see if it n be converted to an integer.
+		 * if th cannot a popup message is displayed.
+		 * @param
+		 */
+	}
+
+	private boolean isInteger(String potentialValue)
+	{
+		boolean validInteger = false;
+		try
+		{
+			int testValue = Integer.parseInt(potentialValue);
+			validInteger = true;
+		} catch (NumberFormatException notIntegerError)
+		{
+			display.displayMessage(notIntegerError.getMessage());
+			display.displayMessage("You need to type an int!");
+
+		}
+		return validInteger;
+	}
+	
+	private void learnLists()
+	{
+		display.displayMessage("This ise size of the list" + thingyLists.size());
+		thingyList.add(firstThingy);
+		display.displayMessage("This is the size of the list" + thingyList.size);
+	}
 }
+
